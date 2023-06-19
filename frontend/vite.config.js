61f1cc2b100139892dcd8cd31d0e2f860748e8e4
Citrom333 <citrom333@gmail.com>
2023-06-19 16:54:08 +0200

    feat: add proxy configurations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://localhost::7146',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false
+      },
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 5173,
+  },
 })
