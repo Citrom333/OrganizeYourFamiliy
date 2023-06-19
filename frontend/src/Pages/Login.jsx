4c646e612eb3a52ec56fa37ba89f4377d35014df
Citrom333 <citrom333@gmail.com>
2023-06-19 22:17:12 +0200

    feat: handle login for families

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,10 +1,39 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react";
 
 function Login() {
-    let navigate = useNavigate();
-    const handleLogin = (e) => {
-        e.preventDefault();
-        navigate('/MainFamilyPage');
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const fetchTest = () =>
+        fetch("/api/Family", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        }).then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+            });
+    const fetchLogin = (familyname, password) => {
+        fetch("/api/Family/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: familyname, password: password }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                localStorage.setItem("familyName", data.name);
+                data == null
+                    ? navigate("/")
+                    : navigate("/MainFamilyPage");
+            });
+    };
+    const handleLogin = () => {
+        console.log(name);
+        console.log(password);
+        localStorage.clear();
+        fetchTest();
+        fetchLogin(name, password);
     }
     return (
         <>
@@ -13,13 +42,13 @@ function Login() {
                 <label>
                     <p>Family identifier</p>
                     <input
-
+                        onChange={(e) => setName(e.target.value)}
                     />
                 </label>
                 <label>
                     <p>Family password</p>
                     <input
-
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </label>
                 <div>
