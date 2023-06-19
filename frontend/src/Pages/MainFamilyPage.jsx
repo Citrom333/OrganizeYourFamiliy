81f18337469caf7ed5b40ab27d06a38151c3ca6b
Citrom333 <citrom333@gmail.com>
2023-06-19 22:22:01 +0200

    feat: show familymembers on MainFamilyPage

--- frontend/src/Pages/MainFamilyPage.jsx
+++ frontend/src/Pages/MainFamilyPage.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react"
 import { Outlet } from "react-router-dom"
-
+import "../Style.css"
 function MainFamilyPage() {
+    const [members, setMembers] = useState([]);
+    const fetchMembers = () =>
+        fetch("/api/User", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        }).then((response) => response.json())
+            .then((json) => {
+                setMembers(json);
 
+            });
 
+    useEffect(() => {
+        fetchMembers();
+        console.log(members);
+    }, [members.length])
     return (
         <>
             <div>
+
                 <h1>This is my family</h1>
+                <p>{localStorage.getItem("familyName")}</p>
+                <ul>
+                    {members.length > 0 ? members.map(mem => <li key={mem.id}>{mem.name}</li>) : ""}
+                </ul>
                 <Outlet />
+
             </div>
 
         </>
