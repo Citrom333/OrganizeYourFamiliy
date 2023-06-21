191a2ea9062afc13cca1855eb5bfad2731a072d0
Citrom333 <citrom333@gmail.com>
2023-06-21 10:16:26 +0200

    feat: add footer

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return <a href="https://www.freepik.com/free-vector/miscellaneous-people-avatar-collection_1176018.htm#query=avatars&position=29&from_view=keyword&track=sph">Avatar images by kubanek on Freepik</a>
+}
