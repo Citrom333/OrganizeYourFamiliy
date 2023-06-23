1ff1da5e8b0ef3f940dec8e17239c6c950c31f7f
Citrom333 <citrom333@gmail.com>
2023-06-23 08:52:29 +0200

    feat: make working sign up page

--- frontend/src/Pages/Singup.jsx
+++ frontend/src/Pages/Singup.jsx
@@ -1,33 +1,79 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 function SignUp() {
-
-
+    const [message, setMessage] = useState("");
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (name === "" || password === "" || password2 === "") {
+            message === "" ? setMessage("Please fill the fields correctly") : "";
+        }
+        else {
+            try {
+                let res = await fetch("/api/Family", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        name: name,
+                        password: password2,
+                        familyMembers: []
+                    }),
+                });
+                if (res.status === 200) {
+                    setName("");
+                    setPassword("");
+                    setPassword2("");
+                    setMessage("Family created successfully");
+                } else {
+                    setMessage("Some error occured");
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    };
+    const handlePasswordConfim = (e) => {
+        if (e.target.value === password) {
+            setPassword2(e.target.value)
+            setMessage("")
+        }
+        else {
+            setMessage("The passwords are different")
+        }
+    }
     return (
         <>
             <div>
                 <h1>Sign up</h1>
-                <label>
-                    <p>Family identifier</p>
-                    <input
-
-                    />
-                </label>
-                <label>
-                    <p>Family password</p>
-                    <input
-
-                    />
-                </label>
-                <label>
-                    <p>Confirm password</p>
-                    <input
-
-                    />
-                </label>
-                <div>
-                    <button >
-                        Sign up
-                    </button>
-                </div>
+                <form className="form" onSubmit={handleSubmit}>
+                    <label>
+                        <p>Family identifier</p>
+                        <input
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        <p>Family password</p>
+                        <input
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        <p>Confirm password</p>
+                        <input
+                            onChange={handlePasswordConfim}
+                        />
+                    </label>
+                    <div >
+                        <input className="submit" type="submit" value="Sign up" />
+                    </div>
+                    <div><p>{message}</p></div>
+                </form>
                 <div>
                     <a href="/">
                         <button >
