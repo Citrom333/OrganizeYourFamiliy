3b668e589eeafb9666b0745a786f21a8219ab7ec
Citrom333 <citrom333@gmail.com>
2023-09-13 11:46:44 +0200

    refactor: rename task to todo

--- backend/Model/Entities/Reward.cs
+++ backend/Model/Entities/Reward.cs
@@ -9,7 +9,7 @@ public class Reward
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
     public long Id { get; set; }
-    public TaskType Type { get; set; }
+    public ToDoType Type { get; set; }
     public int level { get; set; }
     public DateTime Date { get; set; }
     public User Owner { get; set; }
