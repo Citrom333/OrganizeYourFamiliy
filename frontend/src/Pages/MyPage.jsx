6c98e7418ed632829ebb037d949cf526bf4293b5
Citrom333 <citrom333@gmail.com>
2023-10-02 19:09:29 +0200

    refactor: fetch in mypage, and give props to components

--- frontend/src/Pages/MyPage.jsx
+++ frontend/src/Pages/MyPage.jsx
@@ -1,14 +1,28 @@
 import ToDos from "../Components/ToDos"
 import Calendar from "../Components/Calendar"
+import { useState, useEffect } from "react";
 function MyPage() {
+    const [toDos, setToDos] = useState([]);
+    let userId = localStorage.getItem("userId");
+    const fetchToDos = () =>
+        fetch(`/api/ToDo/${userId}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        }).then((response) => response.json())
+            .then((json) => {
+                setToDos(json);
 
+            });
 
+    useEffect(() => {
+        fetchToDos();
+    }, [toDos.length])
     return (
         <>
             <div className="myPage">
                 <h1>My page</h1>
-                <ToDos />
-                <Calendar />
+                <ToDos toDos={toDos} setToDos={setToDos} />
+                <Calendar toDos={toDos} setToDos={setToDos} />
             </div>
 
         </>
