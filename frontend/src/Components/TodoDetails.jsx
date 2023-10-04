a9de19d839469e91343bb4c6edd8fde7826cb324
Citrom333 <citrom333@gmail.com>
2023-10-04 20:24:11 +0200

    feat: add new method (add or subtract rewardpoint after clicking checkbox)

--- frontend/src/Components/TodoDetails.jsx
+++ frontend/src/Components/TodoDetails.jsx
@@ -1,7 +1,25 @@
 import { useState, useEffect } from "react"
 export default function TodoDetails(props) {
     const [isChecked, setIsChecked] = useState(props.toDo.ready)
-
+    const fetchRewardpoints = async () => {
+        let point = !isChecked ? props.toDo.rewardPoint : -1 * props.toDo.rewardPoint;
+        try {
+            let res = await fetch(`/api/user/RewardPoint/${localStorage.getItem("userId")}/${point}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: props.toDo.type.toString(),
+            });
+            if (res.status === 200) {
+                console.log("Reward added");
+            } else {
+                console.log("Some error occured");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
     const handleCheckboxChange = async () => {
         console.log(isChecked);
         setIsChecked(!isChecked);
@@ -24,7 +42,7 @@ export default function TodoDetails(props) {
             });
             if (res.status === 200) {
                 console.log("Todo updated");
-
+                fetchRewardpoints();
             } else {
                 console.log("Some error occured");
             }
@@ -41,7 +59,7 @@ export default function TodoDetails(props) {
             <h3>Description: {props.toDo.description}</h3>
             <h3>Deadline: {props.toDo.deadline.split("T")[0]}</h3>
             <form>
-                <label for="ready">Ready? :   </label><input type="checkbox" id="ready" checked={isChecked}// A checkbox állapotának beállítása az állapot alapján
+                <label for="ready">Ready? :   </label><input type="checkbox" id="ready" checked={isChecked}
                     onChange={handleCheckboxChange} />
             </form>
             <button onClick={e => props.setSelected("")}>Back</button>
