eacf10f361051f1b97386b85b0346db474c35023
Citrom333 <citrom333@gmail.com>
2023-10-11 17:20:22 +0200

    feat: add modal

---
+++ frontend/src/Components/Modal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+import "../Modal.css"
+const Modal = ({ isOpen, onClose, child }) => {
+    if (!isOpen) return null;
+    return (
+        <div className="modal-overlay">
+            <div className="modal">
+                <div>
+                    <button className="close-button" onClick={onClose}>X</button>
+                </div>
+                <div>{child}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
