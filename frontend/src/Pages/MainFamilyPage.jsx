1b42470526ca96b5b94aa1b6f02514cbcaa11c28
Citrom333 <citrom333@gmail.com>
2023-10-11 18:08:54 +0200

    feat: add programdetail modal to main page

--- frontend/src/Pages/MainFamilyPage.jsx
+++ frontend/src/Pages/MainFamilyPage.jsx
@@ -4,12 +4,17 @@ import "../Style.css"
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import Calendar from "../Components/Calendar";
+import Modal from "../Components/Modal";
 import AddNewProgram from "../Components/AddNewProgram";
+import ProgramDetails from "../Components/ProgramDetails";
 function MainFamilyPage() {
     const location = useLocation();
     const [members, setMembers] = useState([]);
     const [programs, setPrograms] = useState([]);
-    const [isOpen, setIsOpen] = useState(false);
+    const [addProgIsOpen, setAddProgIsOpen] = useState(false);
+    const [progDetailIsOpen, setProgDetailIsOpen] = useState(false);
+    const [change, setChange] = useState(false);
+    const [selectedProg, setSelectedProg] = useState("");
     const fetchMembers = () =>
         fetch("/api/User", {
             method: "GET",
@@ -28,14 +33,16 @@ function MainFamilyPage() {
                 setPrograms(json);
 
             });
-    const handleClick = (e) => {
-
-        console.log("");
+    const handleClick = (id, type) => {
+        setSelectedProg(programs.find(p => p.id == id))
+        setProgDetailIsOpen(true)
     }
     useEffect(() => {
         fetchMembers();
         fetchPrograms();
-    }, [members.length, programs.length])
+        console.log(change)
+        setChange(false);
+    }, [members.length, programs.length, change])
     return (
         <>
             <div>
@@ -47,9 +54,10 @@ function MainFamilyPage() {
                                 {members.length > 0 ? members.map(mem => <div key={mem.id}><img className="avatarPic" src={mem.avatarPic} /><div>{mem.name}</div></div>) : ""}
                             </div>
                         </div>
-                            <AddNewProgram isOpen={isOpen} onClose={e => setIsOpen(false)} users={members} />
-                            <button onClick={e => setIsOpen(true)}>Add new program</button>
-                            <div><Calendar isMainPage={true} toDos={[]} handleClick={e => handleClick(e)} toDo={""} programs={programs} /></div>
+                            <Modal isOpen={addProgIsOpen} onClose={e => setAddProgIsOpen(false)} child={<AddNewProgram users={members} setAddedNew={setChange} change={change} />} />
+                            <Modal isOpen={progDetailIsOpen} onClose={e => setProgDetailIsOpen(false)} child={<ProgramDetails program={selectedProg} setSelected={setSelectedProg} handleUpdate={prog => updateFunction(prog)} handleDelete={prog => deleteFunction(prog)} />} />
+                            <button onClick={e => setAddProgIsOpen(true)}>Add new program</button>
+                            <div><Calendar isMainPage={true} toDos={[]} handleClick={(id, type) => handleClick(id, type)} toDo={""} programs={programs} /></div>
                         </div> :
                         ""}
                     <Outlet />
