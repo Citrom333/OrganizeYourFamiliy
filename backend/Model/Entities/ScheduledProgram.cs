ad70d93ec649fd2af348649756f70674cb01243c
Citrom333 <citrom333@gmail.com>
2023-10-11 18:09:53 +0200

    feat: add place to Scheduled programs

--- backend/Model/Entities/ScheduledProgram.cs
+++ backend/Model/Entities/ScheduledProgram.cs
@@ -12,6 +12,7 @@ public class ScheduledProgram
     public string? Name { get; set; }
     public DateTime? Start { get; set; }
     public DateTime? End { get; set; }
+    public string? Place { get; set; }
     public decimal? Cost { get; set; }
     public List<User>? Participants { get; set; }
 }
