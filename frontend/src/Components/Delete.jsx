c7511b79f4c87fbb6b7fbf91d9b1a7e76a315757
Citrom333 <citrom333@gmail.com>
2023-10-12 14:37:44 +0200

    feat: add delete component

---
+++ frontend/src/Components/Delete.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+export default function Delete(props) {
+    const [confirmed, setConfirmed] = useState(false);
+    const [message, setMessage] = useState("")
+    const finalDelete = async () => {
+        setConfirmed(true);
+        try {
+            let url = props.type === "program" ? `/api/ScheduledProgram/${props.toDelete.id}` : `/api/ToDo/${props.toDelete.id}`;
+            let res = await fetch(url, {
+                method: "DELETE"
+            })
+            if (res.status === 200) {
+                props.setChange(true);
+                setMessage("Successfully deleted")
+            } else {
+                console.log(res.status);
+                setMessage("Some error occured");
+            }
+        }
+        catch (err) {
+            setMessage("Error")
+        }
+    }
+    return (
+        <div>
+            {confirmed ?
+                <div><h1>{message}</h1></div>
+                : <div>
+                    <h2>Do you really want to delete?</h2>
+                    <button onClick={finalDelete}>Yes</button>
+                    <button
+                    // onClick={e => setConfirmed(false)}
+                    >No</button>
+                </div>
+            }
+        </div>
+    )
+}
