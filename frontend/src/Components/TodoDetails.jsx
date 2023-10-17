4005c544deb9d628ba699c9b7b29c369ada36a2a
Citrom333 <citrom333@gmail.com>
2023-10-17 20:46:38 +0200

    fix: change "for" to "htmlFor" in label

--- frontend/src/Components/TodoDetails.jsx
+++ frontend/src/Components/TodoDetails.jsx
@@ -21,7 +21,6 @@ export default function TodoDetails(props) {
         }
     }
     const handleCheckboxChange = async () => {
-        console.log(isChecked);
         setIsChecked(!isChecked);
         try {
             let res = await fetch("/api/ToDo", {
@@ -59,7 +58,7 @@ export default function TodoDetails(props) {
             <h3>Description: {props.toDo.description}</h3>
             <h3>Deadline: {props.toDo.deadline.split("T")[0]}</h3>
             <form>
-                <label for="ready">Ready? :   </label><input type="checkbox" id="ready" checked={isChecked}
+                <label htmlFor="ready">Ready? :   </label><input type="checkbox" id="ready" checked={isChecked}
                     onChange={handleCheckboxChange} />
             </form>
             <button onClick={e => props.setSelected("")}>Back</button>
