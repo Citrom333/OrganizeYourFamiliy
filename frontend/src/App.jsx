dbeb19d7f010169b1a09965a3a0e26ec9784ce2f
Citrom333 <citrom333@gmail.com>
2023-10-19 22:26:25 +0200

    feat: add flags to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,35 @@
-
+import data from "./translator.json"
 import './App.css'
 import { Outlet, useLocation } from "react-router-dom";
 import { useState, useEffect } from 'react';
-
+import "./Flags.css"
 function App() {
+  const [language, setLanguage] = useState("English");
+  const [showFlags, setShowFlags] = useState(false);
+  const flags = ["English", "Hungarian", "Deutsch", "Français", "Italiano", "Español"]
   const location = useLocation();
-  // useEffect(() => {
-  //   console.log(location);
-  // }, []);
+  useEffect(() => {
+    console.log(language);
+    localStorage.setItem("language", language);
+  }, [language]);
   return (
     <>
+      <div class="flag-dropdown-content" onMouseEnter={() => setShowFlags(true)}
+        onMouseLeave={() => setShowFlags(false)}>
+        <img className="flag language" src={`../public/images/flags/${language}.png`} ></img>
+        {showFlags ? flags.map(flag => <img className={language === flag ? "flag hidden" : "flag other"} onClick={e => setLanguage(flag)} src={`../public/images/flags/${flag}.png`} ></img>) : ""}
+      </div>
       <div>
         {location.pathname === "/" || location.pathname === "" ? <div>
-          <h1>Hello! This is the Great Family Organizer</h1>
-          <h3>Does your family have an account?</h3>
+          <h1>{data["HelloThisistheGreatFamilyOrganizer"][language]}</h1>
+          <h3>{data["Doesyourfamilyhaveanaccount?"][language]}</h3>
           <a href="/login">
-            <button type="button">Log in</button>
+            <button type="button">{data["Log in"][language]}</button>
           </a>
-          <h3>If not, register now</h3>
+          <h3>{data["If not, register now"][language]}</h3>
           <div>
             <a href="/signup">
-              <button type="button">Sign up</button>
+              <button type="button">{data["Sign up"][language]}</button>
             </a>
           </div>
         </div> :
