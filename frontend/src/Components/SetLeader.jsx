0d76e7878c636761a3e05a1890adc73e9d025647
Citrom333 <citrom333@gmail.com>
2023-10-20 22:22:28 +0200

    fix: remove buttons if question is answered (setLeader)

--- frontend/src/Components/SetLeader.jsx
+++ frontend/src/Components/SetLeader.jsx
@@ -2,7 +2,8 @@ import data from "../translator.json"
 import { useState } from "react";
 export default function SetLeader(props) {
     const language = localStorage.getItem("language");
-    const [message, setMessage] = useState("")
+    const [message, setMessage] = useState("");
+    const [answered, setAnswered] = useState(false);
     const setAsLeader = async () => {
         try {
             let res = await fetch(`/api/Leader/${props.familyId}/${props.userId}`, {
@@ -30,8 +31,9 @@ export default function SetLeader(props) {
                 <h3>{data["Would you like to be set"][language]}</h3>
                 <h3>{data["as the leader of the family?"][language]}</h3>
             </div>
-            <button onClick={setAsLeader}>{data["Yes"][language]}</button>
-            <button onClick={props.onClose}>{data["No"][language]}</button>
+            {answered ? "" : <><button onClick={e => { setAsLeader(); setAnswered(true) }}>{data["Yes"][language]}</button>
+                <button onClick={props.onClose}>{data["No"][language]}</button></>}
+
             <p>{message}</p>
         </div>
     )
