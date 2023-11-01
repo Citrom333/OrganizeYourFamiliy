74678179a870ba8437679412d79a3e2fc3ee592f
Citrom333 <citrom333@gmail.com>
2023-11-01 21:44:50 +0100

    refactor: extract handleLogout

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -7,26 +7,15 @@ export default function Navbar() {
     const navigate = useNavigate();
     useEffect(() => {
     }, [language])
-    const handleLogout = async () => {
-        try {
-            let res = await fetch("/api/Family/logout", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                }),
-            });
-            if (res.status === 200) {
-                localStorage.clear();
-            } else {
-                console.log(data["Some error occured"][language]);
-            }
-        } catch (err) {
-            console.log(err);
-        }
-        navigate("/");
-    }
+
+    const navbuttons = (<><div className="navbuttons">
+        {localStorage.getItem("isAdult") == "false" ? "" : <div> <a href="/MainFamilyPage/AddFamilyMember"><button>{data["Add new member"][language]}</button></a></div>}
+        {localStorage.getItem("userName") !== "" && localStorage.getItem("userName") !== null ?
+            <div> <a href="/MainFamilyPage/MyPage"><button>{data["profile of "][language]}{localStorage.getItem("userName")}{data["'s profile"][language]}</button></a></div>
+            :
+            <div><a href="/MainFamilyPage/LoginAsFamilyMember"><button>{data["Member login"][language]}</button></a></div>}
+        <div><button onClick={() => handleLogout(navigate)}>{data["Logout"][language]}</button></div>
+    </div></>)
     return (
         <div className="navbar">
             <div><a href="/MainFamilyPage"><p>{data["This is "][language]}{localStorage.getItem("familyName")} {data["family"][language]}</p></a></div>
