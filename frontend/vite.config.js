8409b7564aa27a3d906cf18c6812476962c3be20
Citrom333 <citrom333@gmail.com>
2023-11-14 15:33:53 +0100

    fix: fix proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: 'https://backend:7176',
+        target: 'https://familyorganizer.xyz:7176',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false
